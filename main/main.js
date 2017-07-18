6ac65e73dd12f304f04d92c2b884ba67d0ffb8c8
313183373 <313183373@qq.com>
2017-07-18 03:29:44 +0000

    finished 1

--- main/main.js
+++ main/main.js
@@ -1,9 +1,27 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(a,b) {
+    if(a<b){
+        return caclRemaindar(b,a);
+    }
+    return a%b;
+};
 
-function caclSum() {};
+function caclSum(a) {
+    let ans=0;
+    for(let i=0;i<a.length;i++){
+        ans+=a[i];
+    }
+    return ans;
+};
 
-function caclSumInConditon() {};
+function caclSumInConditon(a,b) {
+    let ans=0;
+    for(let i=0;i<a.length;i++){
+        if(a[i]<b)
+            ans+=a[i];
+    }
+    return ans;
+};
 
 module.exports = {
     caclRemaindar,
